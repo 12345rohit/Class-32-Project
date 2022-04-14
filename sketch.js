f8be6165273508b2bf50942df5276c59e1dcd112
12345rohit <96622748+12345rohit@users.noreply.github.com>
2022-04-14 16:15:00 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,71 +1,181 @@
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body;
+const Constraint = Matter.Constraint;
 
-var ball,groundObj,leftSide,rightSide;
-var world;
-var radius = 70;
+var engine, world;
+var canvas;
+var palyer, playerBase;
+var computer, computerBase;
 
+//Declare an array for arrows playerArrows = [ ]
+var playerArrows = [];
+var computerArrows = []
+var arrow;
+var playerArcherLife = 3;
+var computerArcherLife = 3;
 function preload(){
-//find the bug in the below code
-	dustbinImg = loadImage("dustbin.png");
-	paperImg = loadImage("paper.png");
-
+  backgroundImg = loadImage("assets/background.gif")
 }
 
 
 function setup() {
-	createCanvas(1600, 700);
-	rectMode(CENTER);
+  canvas = createCanvas(windowWidth, windowHeight);
 
-	engine = Engine.create();
-	world = engine.world;
+  engine = Engine.create();
+  world = engine.world;
 
-	var ball_options={
-		isStatic:false,
-		restitution:0.3,
-		density:0.4
-	}
+  playerBase = new PlayerBase(300, random(450, height - 300), 180, 150);
+  player = new Player(285, playerBase.body.position.y - 153, 50, 180);
+  playerArcher = new PlayerArcher(
+    340,
+    playerBase.body.position.y - 180,
+    120,
+    120
+  );
 
-	ball = Bodies.circle(260,100,radius/2.6,ball_options);
-	World.add(world,ball);
+  computerBase = new ComputerBase(
+    width - 300,
+    random(450, height - 300),
+    180,
+    150
+  );
+  computer = new Computer(
+    width - 280,
+    computerBase.body.position.y - 153,
+    50,
+    180
+  );
+  computerArcher = new ComputerArcher(
+    width - 340,
+    computerBase.body.position.y - 180,
+    120,
+    120
+  );
+  //Function to manage computer Arrows
+  handleComputerArcher(); 
 
-	ground=new Ground(width/2,670,width,20);
-	leftSide = new Ground(1100,600,10,120);
-	rightSide = new Ground(1270,600,10,120);
-	bottomSide = new Ground(1185,650,150,20);
 
-	Engine.run(engine);
+}
+
+function draw() {
+  background(189);
+
+  //write a correct line of code to display the background image
+  image(backgroundImg,0,0,width,height);
+
+  Engine.update(engine);
+
+  // Title
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("EPIC ARCHERY", width / 2, 100);
+
+ 
+  playerBase.display();
+  player.life();
+
+  player.display();
   
+
+  computerBase.display();
+  computer.display();
+  computer.life();
+
+  playerArcher.display();
+  computerArcher.display()
+
+ // Use for loop to display arrow using showArrow() function
+ for (var i = 0; i < playerArrows.length; i++) {
+  showArrows(i, playerArrows);
+}
+
+for (var i = 0; i < computerArrows.length; i++) {
+  showArrows(i, computerArrows);
 }
 
 
-function draw() {
-	background(200);
-	rectMode(CENTER);
-
-
-	ground.display();
-	leftSide.display();  
-	rightSide.display();
-	bottomSide.display();
-
-	
-	imageMode(CENTER);
-		//use image() command to add paper image to the ball
-image(paperImg,ball.position.x,ball.position.y,radius,radius);
-
-	// use image() command to add dustbin image in the canvas.
-	image(dustbinImg,1185, 570, 200,200);
-	
+//Call functions to detect collision for player and computer
 
 }
 
 function keyPressed() {
-  	if (keyCode === UP_ARROW) {
 
-		Matter.Body.applyForce(ball,ball.position,{x:60.85,y:-40.85});
-    
-  	}
+  if(keyCode === 32){
+    // create an arrow object and add into an array ; set its angle same as angle of playerArcher
+    var posX = playerArcher.body.position.x;
+    var posY = playerArcher.body.position.y;
+    var angle = playerArcher.body.angle+PI/2;
+
+    var arrow = new PlayerArrow(posX, posY, 100, 10);
+
+    arrow.trajectory = [];
+    Matter.Body.setAngle(arrow.body, angle);
+    playerArrows.push(arrow);
+
+  }
 }
+
+function keyReleased () {
+
+  if(keyCode === 32){
+    //call shoot() function for each arrow in an array playerArrows
+    if (playerArrows.length) {
+      var angle = playerArcher.body.angle+PI/2;
+      playerArrows[playerArrows.length - 1].shoot(angle);
+    }
+  }
+
+}
+//Display arrow and Tranjectory
+function showArrows(index, arrows) {
+  arrows[index].display();
+  
+    
+  
+ 
+
+}
+
+function handleComputerArcher() {
+  if (!computerArcher.collapse && !playerArcher.collapse) {
+    setTimeout(() => {
+      var pos = computerArcher.body.position;
+      var angle = computerArcher.body.angle;
+      var moves = ["UP", "DOWN"];
+      var move = random(moves);
+      var angleValue;
+
+      if (move === "UP") {
+        angleValue = 0.1;
+      } else {
+        angleValue = -0.1;
+      }
+      angle += angleValue;
+
+      var arrow = new ComputerArrow(pos.x, pos.y, 100, 10, angle);
+
+      Matter.Body.setAngle(computerArcher.body, angle);
+      Matter.Body.setAngle(computerArcher.body, angle);
+
+      computerArrows.push(arrow);
+      setTimeout(() => {
+        computerArrows[computerArrows.length - 1].shoot(angle);
+      }, 100);
+
+      handleComputerArcher();
+    }, 2000);
+  }
+}
+
+function handlePlayerArrowCollision() {
+// Write code to detect collision between player arrow and opponent
+
+
+}
+
+function handleComputerArrowCollision() {
+  //Write code to detect collision between computer arrow and opponent
+}
+
